8c163f0a046b1d3ffb403eba38219b38dc24f839
zzzatoox <127483506+zzzatoox@users.noreply.github.com>
2023-12-22 09:29:01 +0500

    Commit #1

---
+++ projects/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    technology = models.CharField(max_length=20)
+    image = models.FileField(upload_to="project_images/", blank=True)
+
+
+    def __str__(self):
+        return self.title
